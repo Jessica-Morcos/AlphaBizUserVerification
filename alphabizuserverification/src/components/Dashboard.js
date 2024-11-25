e2e289d1d3bff538ccbe23440a9395b0f11904c1
Jessica-Morcos <132419894+Jessica-Morcos@users.noreply.github.com>
2024-11-25 14:59:44 -0500

    dashboard and logout updated

--- alphabizuserverification/src/components/Dashboard.js
+++ alphabizuserverification/src/components/Dashboard.js
@@ -1,11 +1,6 @@
 import React from "react";
 import { useContext } from "react"
 import { UserContext } from "../context/UserContext"
-import { useNavigate } from "react-router-dom";
-
-import { useState } from "react";
-import { toast } from 'react-hot-toast';
-import axios from "axios";
 import Logout from './Logout'
 
 const Dashboard = () => {
