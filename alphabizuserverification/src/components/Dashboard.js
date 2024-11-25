0a0c09b8a49c0b144554a76d5cd1e1df249bd34b
Jessica-Morcos <132419894+Jessica-Morcos@users.noreply.github.com>
2024-11-25 15:53:26 -0500

    dashboard and logout stylings

--- alphabizuserverification/src/components/Dashboard.js
+++ alphabizuserverification/src/components/Dashboard.js
@@ -1,29 +1,29 @@
 import React from "react";
-import { useContext } from "react"
-import { UserContext } from "../context/UserContext"
-import Logout from './Logout'
+import { useContext } from "react";
+import { UserContext } from "../context/UserContext";
+import Logout from './Logout';
 import "../styles/Dashboard.css"; 
+
 const Dashboard = () => {
     const userContext = useContext(UserContext);
     if (!userContext) {
         return <div>Loading...</div>;
-      }
-    
-      const { user } = userContext;
+    }
+
+    const { user } = userContext;
     return (
         <div className="dash__container">
-          <div className="article__container">
-            <div className="article">
-              <h2>welcome back,</h2>
-              <h1>{user?.name}</h1>
+            <div className="logout__button">
+                <Logout />
             </div>
-          </div>
-          <div className="logout__button">
-             <Logout />
-          </div>
-          
+            <div className="article__container">
+                <div className="article">
+                    <h2>welcome back,</h2>
+                    <h1>{user?.name}</h1>
+                </div>
+            </div>
         </div>
-      )
-}
+    );
+};
 
-export default Dashboard
+export default Dashboard;
